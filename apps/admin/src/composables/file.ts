49c2ff2544ce7d9f7a27b7b6064c282e8bd87968
ruanbw <1813967922@qq.com>
2026-03-09 22:30:08 +0800

    fix: update file URL retrieval to use environment variable

--- apps/admin/src/composables/file.ts
+++ apps/admin/src/composables/file.ts
@@ -1,12 +1,8 @@
-import { useAppConfig } from '@vben/hooks';
-
-const { fileURL } = useAppConfig(import.meta.env, import.meta.env.PROD);
-
 /**
  * 获取文件URL
  * @param key 文件key
  * @returns 完整的文件URL
  */
 export function getFileUrl(key: string) {
-  return `${fileURL}${key}`;
+  return `${import.meta.env.VITE_GLOB_FILE_URL}${key}`;
 }
